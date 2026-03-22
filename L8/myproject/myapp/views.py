4897176e4dfe6baac4daa0b467e3a809da296331
Maakham <109505483+Maakham@users.noreply.github.com>
2026-03-22 10:18:03 +0700

    Add files via upload

---
+++ L8/myproject/myapp/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+
+
+def index(request):
+    return render(request, 'myapp/index.html')
+
+def form(request):
+    return render(request, 'myapp/form.html')
+
+def about(request):
+    return render(request, 'myapp/about.html')
