72094d43215bd8bf3862f5f15ca26ac0d1de48a4
Maakham <109505483+Maakham@users.noreply.github.com>
2026-03-22 09:52:25 +0700

    Add files via upload

---
+++ L6/app.py
@@ -0,0 +1,11 @@
+# 1. พิมพ์ตัวเลขและข้อความร่วมกัน
+age = 25
+print("อายุของฉันคือ", age)
+
+# 2. ใช้ f-string แทรกค่าตัวแปร
+name = "Nontawat riancharoen"
+age = 23
+print(f"สวัสดี {name}, อายุ {age} ปี")
+
+# 3. ใช้ sep ก
+print("Python", "Java", "C++", sep=" | ")
